a528024aa0ac17af61d67029a2010a608bf5b814
Denis <d.holubchikov@gmail.com>
2021-10-02 16:39:00 +0300

    added section about

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(() => {
+
+	$('.header-navbar__btn').on('click', function () {
+	  $(this).toggleClass('header-navbar__btn--active');
+	  $('.header-navbar__list')
+		.stop(true, true)
+		.slideToggle(500);
+	});
+
+  });
